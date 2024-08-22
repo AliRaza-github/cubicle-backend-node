7844138dbbef4612d45f9521fbe327ad7d843243
Ali Raza <123078445+AliRaza-github@users.noreply.github.com>
2024-08-22 12:21:01 +0500

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const serverless = require('serverless-http');
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
